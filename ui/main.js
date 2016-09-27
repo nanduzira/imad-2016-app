f0bd87d217e441128d3bf2c6f38d0aa6c789ed48
nanduzira <anandsunil578@gmail.com>
2016-09-27 19:28:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,9 +9,9 @@ element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft +10;
+    marginLeft = marginLeft +1;
     img.style.marginleft = marginLeft + 'px';
 }
 img.onClick = function() {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
 };
