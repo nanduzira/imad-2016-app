37076087117cca454d0fd05b887dcb42ed773f45
nanduzira <anandsunil578@gmail.com>
2016-09-27 21:40:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/submit-name/:name', function(req, res) {
     // get the name from the request
     var name = req.params.name; // TODO
-    
+    var names;
     names.push(name);
     // JSON: Javascript Object Notation
     res.send(JSON.stringify(names)); // TODO
